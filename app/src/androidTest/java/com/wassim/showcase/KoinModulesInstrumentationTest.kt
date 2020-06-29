7c8b103af86dbe2d217d73df82ce27889d05e1e8
wassim <beltaief.wassim@gmail.com>
2020-06-29 14:33:26 +0200

    fix tests

--- app/src/androidTest/java/com/wassim/showcase/KoinModulesInstrumentationTest.kt
+++ app/src/androidTest/java/com/wassim/showcase/KoinModulesInstrumentationTest.kt
@@ -1,6 +1,9 @@
 package com.wassim.showcase
 
+import com.wassim.showcase.di.appModule
+import com.wassim.showcase.di.databaseModule
 import com.wassim.showcase.di.imageLoader
+import com.wassim.showcase.di.networkModule
 import org.junit.Test
 import org.koin.test.KoinTest
 import org.koin.test.check.checkModules
@@ -9,6 +12,11 @@ class KoinModulesInstrumentationTest : KoinTest {
 
     @Test
     fun checkAllModules() = checkModules {
-        modules(imageLoader)
+        modules(
+            appModule,
+            networkModule,
+            databaseModule,
+            imageLoader
+        )
     }
 }
