66d8cb0ac25831e7d9572c29397db9c8ed1cb399
wassim <beltaief.wassim@gmail.com>
2020-07-02 12:43:22 +0200

    fix tests

--- core/src/main/java/com/wassim/showcase/core/di/ViewModelKey.kt
+++ core/src/main/java/com/wassim/showcase/core/di/ViewModelKey.kt
@@ -2,13 +2,9 @@ package com.wassim.showcase.core.di
 
 import androidx.lifecycle.ViewModel
 import dagger.MapKey
+import kotlin.annotation.AnnotationRetention.RUNTIME
 import kotlin.reflect.KClass
 
-@Target(
-    AnnotationTarget.FUNCTION,
-    AnnotationTarget.PROPERTY_GETTER,
-    AnnotationTarget.PROPERTY_SETTER
-)
-@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 @MapKey
+@Retention(RUNTIME)
 annotation class ViewModelKey(val value: KClass<out ViewModel>)
