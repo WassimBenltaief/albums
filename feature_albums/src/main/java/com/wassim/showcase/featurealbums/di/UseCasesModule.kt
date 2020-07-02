f565869f0d2e68288dab9e9db54d581719fce8a3
wassim <beltaief.wassim@gmail.com>
2020-07-02 12:44:03 +0200

    clean

--- feature_albums/src/main/java/com/wassim/showcase/featurealbums/di/UseCasesModule.kt
+++ feature_albums/src/main/java/com/wassim/showcase/featurealbums/di/UseCasesModule.kt
@@ -1,6 +1,5 @@
 package com.wassim.showcase.featurealbums.di
 
-import androidx.lifecycle.ViewModelProvider
 import com.wassim.showcase.featurealbums.view.item.usecase.GetAlbumInfoUseCase
 import com.wassim.showcase.featurealbums.view.item.usecase.GetAlbumInfoUseCaseImpl
 import com.wassim.showcase.featurealbums.view.item.usecase.SaveAlbumUseCase
@@ -15,11 +14,6 @@ import dagger.hilt.android.components.ActivityComponent
 @Module
 @InstallIn(ActivityComponent::class)
 abstract class UseCasesModule {
-
-    @Binds
-    internal abstract fun bindViewModelFactory(
-        factory: ViewModelFactory
-    ): ViewModelProvider.Factory
 
     @Binds
     abstract fun providesGetAlbumsUseCase(
